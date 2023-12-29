3c12f79487bdbe5ebb0fc22430f7bcae684f999e
hhertout <hugues.hertout@gmail.com>
2023-12-29 13:35:03 +0100

    fix: fix created_at column def in migration file

--- migrations/2023-12-28_1703765604_migration.sql
+++ migrations/2023-12-28_1703765604_migration.sql
@@ -1,11 +1,12 @@
 /* Caution : each query must be separated with empty comment */
 
-CREATE TABLE IF NOT EXISTS "user" (
-                                      id text PRIMARY KEY not null DEFAULT gen_random_uuid(),
-    created_at date,
+CREATE TABLE IF NOT EXISTS "user"
+(
+    id         text PRIMARY KEY not null DEFAULT gen_random_uuid(),
+    created_at date             not null,
     deleted_at date,
-    email varchar(255) not null unique,
-    password varchar(255) not null
+    email      varchar(255)     not null unique,
+    password   varchar(255)     not null
 );
 
 --
